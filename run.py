2931c0933834a6ea8e0561305db5047f8ad20c57
hardikr586 <hbr244@nyu.edu>
2020-09-24 18:00:31 -0400

    C

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import time
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
-
+@app.route('/time')
+def show_time():
+ 
+	seconds = time.time()
+	local_time = time.ctime(seconds)
+	localtime = "Local time:"+ local_time
+	return localtime
 
 app.run(host='0.0.0.0',
         port=8080,
